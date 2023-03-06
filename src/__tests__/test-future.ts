662ef37f3475e58a6df000a1b11b44489e4b4ec3
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2023-03-06 01:17:53 -0800

    Rework bulk tests.

--- src/__tests__/test-future.ts
+++ src/__tests__/test-future.ts
@@ -2,40 +2,80 @@ import { Future } from "../future";
 
 const never = () => new Promise(() => {})
 
+type MethodTags = 'static' | 'constructor' | 'instance';
+
+type StaticFieldName = Exclude<keyof typeof Future, ''>;
+type InstanceFieldName = string & Exclude<keyof Future<any>, 'prototype'>;
+
+type FieldName = StaticFieldName | InstanceFieldName;
+
+interface MethodSpec {
+    name: FieldName;
+    tags: Array<MethodTags>
+}
+
+const methods: Array<MethodSpec> = [
+    {name: "delay", tags: ['static']},
+    {name: "timeout", tags: ['static']},
+    {name: "timeoutFromNow", tags: ['static']},
+    {name: "resolve", tags: ['static']},
+    {name: "reject", tags: ['static']},
+    {name: "cancelled", tags: ['static']},
+    {name: "never", tags: ['static']},
+    {name: "all", tags: ['static']},
+    {name: "allSettled", tags: ['static']},
+    {name: "race", tags: ['static']},
+    {name: "any", tags: ['static']},
+    {name:'then', tags: ['instance']},
+    {name: 'catch', tags: ['instance']},
+    {name: 'finally', tags: ['instance']},
+    {name: 'when', tags: ['instance']},
+    {name: 'onStart',   tags: ['instance']},
+    {name: 'onCancel', tags: ['instance']},
+    {name: 'onTimeout', tags: ['instance']},
+    {name: 'start', tags: ['instance']},
+    {name: 'check', tags: ['instance']},
+    {name: 'cancel', tags: ['instance']},
+    {name: 'start', tags: ['instance']}
+];
+
+const hasTag = (tag: MethodTags) => (method: MethodSpec) => method.tags.includes(tag);
+
 describe("Basic", () => {
     describe("API completeness", () => {
+        test("Future is a function", () =>
+            expect(Future)
+            .toBeInstanceOf(Function));
+        test("Future is a constructor", () =>
+                expect(Future.prototype)
+                .toBeInstanceOf(Object));
         describe("Static Methods", () =>
-            test.each([
-                Future,
-                Future.delay,
-                Future.timeout,
-                Future.timeoutFromNow,
-                Future.resolve,
-                Future.reject,
-                Future.cancelled,
-                Future.never,
-                Future.all,
-                Future.allSettled,
-                Future.race,
-                Future.any
-            ].map(f => ({name: f == Future ? '' : `.${f.name}`, f})))(
-                "Future$name is a function",
-                ({f}) => expect(f).toBeInstanceOf(Function)
+            test.each(methods
+                .filter(hasTag('static'))
+                .map(method => ({name: method.name, method})))(
+                    "Future$name is a function",
+                    spec => expect(Future[spec.name as StaticFieldName]).
+                        toBeInstanceOf(Function)
             ));
-
-        test("Instance Methods", () => {
-            const f = new Future(() => 1);
-            expect(f).toBeInstanceOf(Future);
-            expect(f.then).toBeInstanceOf(Function);
-            expect(f.catch).toBeInstanceOf(Function);
-            expect(f.finally).toBeInstanceOf(Function);
-            expect(f.when).toBeInstanceOf(Function);
-            expect(f.onCancel).toBeInstanceOf(Function);
-            expect(f.onTimeout).toBeInstanceOf(Function);
-            expect(f.start).toBeInstanceOf(Function);
-            expect(f.check).toBeInstanceOf(Function);
-            expect(f.cancel).toBeInstanceOf(Function);
-            expect(f.start).toBeInstanceOf(Function);
+        describe('Instance Methods', () => {
+        const f: Future<number> = new Future(() => 1);
+        const instanceMethods: Array<keyof typeof f> = [
+            'then',
+            'catch',
+            'finally',
+            'when',
+            'onStart',  
+            'onCancel',
+            'onTimeout',
+            'start',
+            'check',
+            'cancel',
+            'start'
+        ];
+        test('InstanceOfFuture', () => expect(f).toBeInstanceOf(Future));
+        test.each((instanceMethods).map(name => ({name, value: f[name]})))
+        ("Method Future.$name", ({name, value}) =>
+            expect(value).toBeInstanceOf(Function));
         });
 
         test('Initial state', () => {
